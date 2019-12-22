f321ab145de6ebe825d62b15523db6a7c2d37dd4
Yassine Iharkachen <y.iharkachen@gmail.com>
2019-12-22 14:27:12 +0100

    tasks index controller and view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.new(content: 'task number 1', check: true).save
+Task.new(content: 'task number 2', check: true).save
+Task.new(content: 'task number 3', check: true).save
+Task.new(content: 'task number 4', check: true).save
+Task.new(content: 'task number 5', check: true).save
+Task.new(content: 'task number 6', check: true).save
+Task.new(content: 'task number 7', check: true).save
+Task.new(content: 'task number 8', check: true).save
+Task.new(content: 'task number 9', check: true).save
+Task.new(content: 'task number 10', check: false).save
+Task.new(content: 'task number 11', check: false).save
+Task.new(content: 'task number 12', check: false).save
+Task.new(content: 'task number 13', check: false).save
