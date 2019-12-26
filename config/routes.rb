f5a46c0379337b596265b040141ef0376d035e90
Yassine Iharkachen <y.iharkachen@gmail.com>
2019-12-26 20:24:17 +0100

    routes fixing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :areas do
-    resources :feedbacks, only: [:new, :create]
     resources :protocols do
-      resources :feedbacks, only: [:new, :create]
       resources :tasks do
         resources :feedbacks, only: [:new, :create]
       end
